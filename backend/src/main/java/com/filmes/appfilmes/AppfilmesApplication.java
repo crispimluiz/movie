24412aed77ce2dc3696c2999d4510a8927b388c9
crispimluiz <crispimluiz@live.com>
2022-01-10 13:15:42 -0300

    first commit

---
+++ backend/src/main/java/com/filmes/appfilmes/AppfilmesApplication.java
@@ -0,0 +1,13 @@
+package com.filmes.appfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfilmesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/filmes/appfilmes/AppfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmes.appfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
