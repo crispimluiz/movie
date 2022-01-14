6d129ac63bd31ac896de529965dab6170b002364
crispimluiz <crispimluiz@live.com>
2022-01-14 14:25:28 -0300

    Show score

--- frontend/src/components/MovieStart/index.tsx
+++ frontend/src/components/MovieStart/index.tsx
@@ -2,14 +2,55 @@ import { ReactComponent as StarFull } from 'assets/img/estrelacheia.svg';
 import { ReactComponent as StarHalf } from 'assets/img/estrelameiacheia.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/estrelavazia.svg';
 import './styles.css'
-function MovieStart() {
+
+type Props = {
+  score: number;
+}
+
+type StarProps = {
+  fill: number;
+}
+
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({ fill }: StarProps) {
+  if (fill === 0) {
+    return <StarEmpty />
+  } else if (fill == 1) {
+    return <StarEmpty />
+  } else {
+    return <StarHalf />
+  }
+}
+
+function MovieStart({ score }: Props) {
+  const fills = getFills(score);
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      <Star fill={fills[0]} />
+      <Star fill={fills[1]} />
+      <Star fill={fills[2]} />
+      <Star fill={fills[3]} />
+      <Star fill={fills[4]} />
     </div>
   );
 }
